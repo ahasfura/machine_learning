b7d72d29603ab527cdd4f75c201fbbd554362f76
Kathryn Evans <klevans@mit.edu>
2015-09-28 21:19:36 -0400

    3.2

--- hw1/src/3_3.m
+++ hw1/src/3_3.m
@@ -1,65 +1,40 @@
 %% plot 1.4 
 
-train = importdata('regress_train.txt');
-test = importdata('regress_test.txt');
-validate= importdata('regress_validate.txt');
+X = importdata('x_train.csv');
+Y = importdata('y_train.csv');
 
-X = train(1,:)';
-Y = train(2,:)';
-xtest= test(1,:)'; 
-ytest= test(2,:)'; 
-xval= validate(1,:)'; 
-yval= validate(2,:)'; 
+xtest = importdata('x_test.csv');
+xtest = importdata('y_test.csv');
+
+xval= importdata('x_val.csv');
+yval= importdata('y_val.csv');
+
+
 %%
 lambdas = linspace(5,6,11);
 for i= 1:10 
 lambda=lambdas(i); 
-    M=0; 
-    [w_ml0]=  ridge_regression(X, Y, M, lambda); 
-    y0t= zeros(size(xtest))+w_ml0; 
-    SSE0(i)= (y0t-ytest)'*(y0t-ytest); 
-    subplot(1,4,1)
-    plot(xtest, y0t, '*')
-    hold on 
-    %plot(X,Y, 'o')
-    plot(xtest, ytest, 'o')
-    title('M=0')
-
+ 
     M=1; 
     [w_ml1]=  ridge_regression(X,Y, M, lambda); 
     y1t= w_ml1(1)+w_ml1(2)*xtest;  
       SSE1(i)= (y1t-ytest)'*(y1t-ytest); 
 
+    y1v= w_ml1(1)+w_ml1(2)*xval;  
+      SSE1V(i)= (y1t-yval)'*(y1t-yval); 
+
     subplot(1,4,2)
-    plot(xtest, y1t, '*')
+    plot(xval, y1t, '*')
      hold on 
     %plot(X,Y, 'o')
 
+    subplot(1,4,1)
+    plot(xtest, y1t, '*')
+     hold on 
+    plot(X,Y, 'o')
+
     plot(xtest, ytest, 'o')
     title('M=1')
-
- M=2; 
-    [w_ml2]=  ridge_regression(X,Y, M, lambda); 
-    y2t= w_ml2(1)+w_ml2(2)*xtest+ w_ml2(3)*xtest.^2; 
-     SSE2(i)= (y2t-ytest)'*(y2t-ytest); 
-
-    subplot(1,4,3)
-    plot(xtest, y2t, '*')
-     hold on 
-    plot(xtest, ytest, 'o')
-    title('M=2')
-
-    M=3; 
-    [w_ml3]=  ridge_regression(X,Y, M, lambda); 
-    y3t= w_ml3(1)+w_ml3(2)*xtest+ w_ml3(3)*xtest.^2+w_ml3(4)*xtest.^3;
-     SSE3(i)= (y3t-ytest)'*(y3t-ytest); 
-
-    subplot(1,4,4)
-    hold on 
-    plot(xtest, y3t, '*')
-    plot(xtest, ytest, 'o')
-    title('M=3')
-
 
  end   
 
@@ -80,13 +55,7 @@ lambda=lambdas(i);
 
     M=1; 
     [w_ml1]=  ridge_regression(X,Y, M, lambda); 
-    y1t= w_ml1(1)+w_ml1(2)*xval;  
-      SSE1V(i)= (y1t-yval)'*(y1t-yval); 
-
-    subplot(1,4,2)
-    plot(xval, y1t, '*')
-     hold on 
-    %plot(X,Y, 'o')
+   
 
     plot(xval, yval, 'o')
     title('M=1')
