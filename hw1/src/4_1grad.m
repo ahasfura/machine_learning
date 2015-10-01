76527141c0ea430a67218b1579db0397c87bcb15
Kathryn Evans <klevans@mit.edu>
2015-10-01 17:35:18 -0400

    final content update

--- hw1/src/4_1grad.m
+++ hw1/src/4_1grad.m
@@ -20,45 +20,46 @@ for i=1:100
     
     M=0; 
     f= @(w_LAD) LAD_error(w_LAD, X, Y, M, lambda); 
-    [w, f_min, int]= grad_descent_nog(.1 , .001, 10^(-4), f, .1);
+    [w, f_min, int]= grad_descent_nog(1 , .001, 10^(-4), f, .1);
     W0(i)= w; 
     y0t= (w)+zeros(size(xval)); 
     e0i=sum(abs(y0t-yval)); %%'*(y0t-yval));
     Error_0(i) = e0i;
     
-   
+  
      M=1; 
     f= @(w_LAD) LAD_error(w_LAD, X, Y, M, lambda); 
-    [w, f_min, int]= grad_descent_nog([.1; .1] , .001, 10^(-4), f, .1);
+    [w, f_min, int]= grad_descent_nog([1; 1] , .001, 10^(-4), f, .1);
     W1(i,:)= w; 
     y1t= (w(1))+w(2)*(xval); 
     e1i=(sum(abs(y1t-yval)));%%'*(y1t-yval));
     Error_1(i) = e1i;
-   
+ 
      M=2; 
     f= @(w_LAD) LAD_error(w_LAD, X, Y, M, lambda); 
-    [w, f_min, int]= grad_descent_nog([.1; .1; .1] , .001, 10^(-4), f, .1);
+    [w, f_min, int]= grad_descent_nog([1; 1; 1] , .001, 10^(-4), f, .1);
     W2(i,:)= w; 
     y2t= (w(1))+w(2)*(xval)+ w(3)*(xval.^2); 
     e2i=sum(abs(y2t-yval)); %%'*(y2t-yval));
     Error_2(i) = e2i;
-    
+   
     M=3; 
     f= @(w_LAD) LAD_error(w_LAD, X, Y, M, lambda); 
-    [w, f_min, int]= grad_descent_nog([.1; .1; .1; .1] , .001, 10^(-4), f, .1);
+    [w, f_min, int]= grad_descent_nog([1; 1; 1; 1] , .001, 10^(-4), f, .1);
     W3(i,:)= w; 
     y3t= (w(1))+w(2)*(xval)+ w(3)*(xval.^2)+w(4)*(xval.^3); 
     e3i=sum(abs(y3t-yval)); %%'*(y2t-yval));
     Error_3(i) = e3i;
     
-    
+   
      M=4; 
     f= @(w_LAD) LAD_error(w_LAD, X, Y, M, lambda); 
-    [w, f_min, int]= grad_descent_nog([.1; .1; .1; .1; .1] , .001, 10^(-4), f, .1);
+    [w, f_min, int]= grad_descent_nog([1; 1; 1; 1; 1] , .001, 10^(-4), f, .1);
     W4(i,:)= w; 
     y4t= (w(1))+w(2)*(xval)+ w(3)*(xval.^2)+w(4)*(xval.^3)+w(5)*(xval.^4); 
     e4i=sum(abs(y4t-yval)); %%'*(y2t-yval));
     Error_4(i) = e4i;
+    %%
  end 
  [min0, i0]= min(Error_0); 
  l0= lambdas(i0); 
@@ -79,7 +80,7 @@ for i=1:100
  %%
  xpred= linspace(-3,2, 100); 
  ypred= w1(1)+w1(2)*xpred; 
- plot(xval,yval, 'o')
+ plot(xtest,ytest, '*k')
  hold on 
  plot(xpred, ypred) 
  
--- hw1/src/rr_test_valdiation.m
+++ hw1/src/rr_test_valdiation.m
@@ -11,61 +11,49 @@ ytest= test(2,:)';
 xval= validate(1,:)'; 
 yval= validate(2,:)'; 
 %%
-lambdas = logspace(-10,0,100);
+lambdas = logspace(-10,1,100);
 
 for i= 1:100 
 lambda=lambdas(i); 
     M=0; 
     [w_ml0]=  ridge_regression(X, Y, M, lambda); 
+    w0(i)=w_ml0; 
     y0t= zeros(size(xval))+w_ml0; 
     SSE0V(i)= (y0t-yval)'*(y0t-yval) ; %%+lambda*(norm(w_ml0))^2); 
-    subplot(1,4,1)
-    plot(xval, y0t, '*')
-    hold on 
-    %plot(X,Y, 'o')
-    plot(xval, yval, 'o')
-    title('M=0')
 
     M=1; 
-    [w_ml1]=  ridge_regression(X,Y, M, lambda); 
+    [w_ml1]=  ridge_regression(X,Y, M, lambda);
+     w1(i,:)=w_ml1; 
+
     y1t= w_ml1(1)+w_ml1(2)*xval;  
       SSE1V(i)= (y1t-yval)'*(y1t-yval); %%+lambda*(norm(w_ml1))^2);; 
 
-    subplot(1,4,2)
-    plot(xval, y1t, '*')
-     hold on 
-    %plot(X,Y, 'o')
-
-    plot(xval, yval, 'o')
-    title('M=1')
+   
 
 
  M=2; 
-    [w_ml2]=  ridge_regression(X,Y, M, lambda); 
+    [w_ml2]=  ridge_regression(X,Y, M, lambda);
+    w2(i,:)=w_ml2; 
+
     y2t= w_ml2(1)+w_ml2(2)*xval+ w_ml2(3)*xval.^2; 
      SSE2V(i)= (y2t-yval)'*(y2t-yval); %%+lambda*(norm(w_ml2))^2);; 
 
-    subplot(1,4,3)
-    plot(xval, y2t, '*')
-     hold on 
-    plot(xval, yval, 'o')
-    title('M=2')
+   
 
 
     M=3; 
-    [w_ml3]=  ridge_regression(X,Y, M, lambda); 
+    [w_ml3]=  ridge_regression(X,Y, M, lambda);
+     w3(i,:)=w_ml3; 
+
     y3t= w_ml3(1)+w_ml3(2)*xval+ w_ml3(3)*xval.^2+w_ml3(4)*xval.^3;
      SSE3V(i)= (y3t-yval)'*(y3t-yval) ; %%+lambda*(norm(w_ml3))^2); 
 
-    subplot(1,4,4)
-    hold on 
-    plot(xval, y3t, '*')
-    plot(xval, yval, 'o')
-    title('M=3')
-
+ 
 
      M=4; 
     [w_ml4]=  ridge_regression(X,Y, M, lambda); 
+    w4(i,:)=w_ml4; 
+
     y4t= w_ml4(1)+w_ml4(2)*xval+ w_ml4(3)*xval.^2+w_ml4(4)*xval.^3+ w_ml4(5)*xval.^4;
      SSE4V(i)= (y4t-yval)'*(y4t-yval); %%+lambda*(norm(w_ml4))^2); 
 
@@ -93,11 +81,25 @@ lambda=lambdas(i);
     l4= lambdas(i4); 
     l5= lambdas(i5); 
 %%
+w_1=w1(i1,:); 
+w_ml2=w2(i2,:); 
+w_ml3=w3(i3,:); 
+w_ml4=w4(i4,:); 
+
 xpred= linspace(-2.5,2); 
- y4t= w_ml4(1)+w_ml4(2)*xpred+ w_ml4(3)*xpred.^2+w_ml4(4)*xpred.^3+ w_ml4(5)*xpred.^4;
-plot (xtest,ytest, 'k*')
+y1t= w_1(1)+w_1(2)*xpred;
+
+y2t= w_ml2(1)+w_ml2(2)*xpred+ w_ml2(3)*xpred.^2;
+
+y3t= w_ml3(1)+w_ml3(2)*xpred+ w_ml3(3)*xpred.^2+w_ml3(4)*xpred.^3;
+
+y4t= w_ml4(1)+w_ml4(2)*xpred+ w_ml4(3)*xpred.^2+w_ml4(4)*xpred.^3+ w_ml4(5)*xpred.^4;
+plot (xval,yval, 'o')
 hold on 
-plot(xpred, y4t, '-')
+plot(xpred, y1t)
+plot(xpred, y2t)
+plot(xpred, y3t)
+plot(xpred, y4t)
 
 %%
 (SSE0+SSE0V)/2
