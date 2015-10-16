5544bbd4aa10aec505edb755d58cbb008c7ce02d
Andres Hasfura <hasfuraa@mit.edu>
2015-10-15 23:07:37 -0400

    problem 2 started

---
+++ hw2/src/find_svm_weights.m
@@ -0,0 +1,49 @@
+function [sol]= find_svm_weights(x, y, C) 
+% function to find the optimal weights for soft svm classification
+% inputs
+% x: data
+% y: classification
+% outputs
+% sol: optimized weight solution
+
+%% set starting x
+
+X0 = zeros(length(x),1); % arbitrarily chosen starting point
+
+%% set bounds on a
+
+LB = zeros(length(x),1); % lower bound on all values of a is 0
+UB = ones(length(x),1) * C; % upper bound on all values of a is C
+
+%% set equation constraint
+
+Aeq = y * y'; % y' * a = 0 TODO: NEED TO FIX THIS ANDRES, not correct Aeq
+beq = zeros(length(x),1);
+
+%% set opts
+
+optim_ver = ver('optim');                                                                                                                                                          
+optim_ver = str2double(optim_ver.Version);
+if optim_ver >= 6
+    opts = optimset('Algorithm', 'interior-point-convex');
+else
+    opts = optimset('Algorithm', 'interior-point', 'LargeScale', 'off', 'MaxIter', 2000);
+end
+
+%% set H and f where 1/2*x'*H*x + f'*x
+
+% TODO: Hessian is not symmetric. Rethink
+H = (y * y') * (x * x'); % this makes a matrix where every entry is t_n * t_m * x_m * x_n (linear kernel)
+f = ones(length(x),1);
+
+%% set A and b where A*x ? b
+
+A = zeros(length(x),length(x)); % both set to zero so this doens't contribute
+b = zeros(length(x),1);
+
+%% run quadratic programming
+
+size(A)
+size(b)
+sol = quadprog(H, f, A, b, Aeq, beq, LB, UB, X0, opts);
+
