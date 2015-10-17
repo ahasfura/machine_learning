9b690185e55599c9d08f0a41aea0f13089a8efa6
Kathryn Evans <klevans@mit.edu>
2015-10-17 15:43:26 -0400

    finished p1

--- hw2/src/p1.m
+++ hw2/src/p1.m
@@ -7,10 +7,10 @@
 
 
 %%
-[test_error1, val_error1]=lr_test('stdev1', [1,2], .1)
-[test_error2, val_error2]=lr_test('stdev2', [3,4], .1)
-[test_error4, val_error4]=lr_test('stdev4', [5,6], .1)
-[test_errorn, val_errorn]=lr_test('nonsep', [7,8], .1)
+[test_error1, val_error1]=lr_test('stdev1', [1,2], 1)
+[test_error2, val_error2]=lr_test('stdev2', [3,4], 1)
+[test_error4, val_error4]=lr_test('stdev4', [5,6], 1)
+[test_errorn, val_errorn]=lr_test('nonsep', [7,8], 1)
 %%
 [test_error1, val_error1]=lr_test('stdev1', [1,2], 100)
 [test_error2, val_error2]=lr_test('stdev2', [3,4], 100)
@@ -34,7 +34,7 @@
 
 %%
 
-[test_error1, val_error1]=lr_test('stdev1', [1,2], 10000)
-[test_error2, val_error2]=lr_test('stdev2', [3,4], 10000)
-[test_error4, val_error4]=lr_test('stdev4', [5,6], 10000)
-[test_errorn, val_errorn]=lr_test('nonsep', [7,8], 10000)
+[test_error1, val_error1]=lr_test('stdev1', [1,2], 2000)
+[test_error2, val_error2]=lr_test('stdev2', [3,4], 2000)
+[test_error4, val_error4]=lr_test('stdev4', [5,6], 2000)
+[test_errorn, val_errorn]=lr_test('nonsep', [7,8], 2000)
